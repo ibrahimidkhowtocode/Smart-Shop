8791f2d05836791990496dc733da2677eb09a05c
IbrahimA <IbrahimA@DESKTOP-H47R601>
2025-04-16 23:31:07 +0300

    Add project files.

---
+++ Smart Shop/AdminForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data.SQLite;
+using System.Windows.Forms;
+
+namespace StoreManagementSystem
+{
+    public partial class AdminForm : Form
+    {
+        public AdminForm()
+        {
+            InitializeComponent();
+            LoadDashboard();
+        }
+
+        private void LoadDashboard()
+        {
+            lblWelcome.Text = "Welcome, Admin! Today is " + DateTime.Now.ToString("dddd, MMMM dd");
+        }
+
+        private void dashboardToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LoadDashboard();
+        }
+
+        private void productsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("We'll add products here later!");
+        }
+
+        // Add similar methods for other menu items
+    }
+}
